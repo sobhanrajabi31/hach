9a96d7e5309b770305a090b861c2145d27c1126d
Sobhan Rajabi <sobhanrajabi31@gmail.com>
2025-12-25 21:26:05 +0330

    Update install.bat

--- Install.bat
+++ Install.bat
@@ -5,21 +5,13 @@ set "TARGET_DIR=%~dp0"
 set "TARGET_DIR=%TARGET_DIR:~0,-1%"
 
 if not exist "%TARGET_DIR%\hach.exe" (
-    echo ERROR: hach.exe not found in:
-    echo %TARGET_DIR%
-    pause
     exit /b 1
 )
 
 echo %PATH% | find /I "%TARGET_DIR%" >nul
-if %errorlevel%==0 (
-    echo Path already exists.
-) else (
-    echo Adding to PATH:
-    echo %TARGET_DIR%
-    setx PATH "%PATH%;%TARGET_DIR%" >nul
-    echo Added successfully.
+if %errorlevel% neq 0 (
+    setx PATH "%PATH%;%TARGET_DIR%" >nul 2>&1
 )
 
-cmd /c del "%~f0"
+cmd /c del "%~f0" >nul 2>&1
 exit
